52abcfa1e69e392d9f4bf775611a01a7c89ff3ba
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-09-25 02:55:23 +0200

    ⏲️ Add nix option and systemd service

--- flake.nix
+++ flake.nix
@@ -44,10 +44,10 @@
           src = ./assets;
           nodejs = nodejs;
         };
-      in {
+      in rec {
         default = erlangPackages.mixRelease {
           inherit version src mixFodDeps;
-          pname = "ravensiris-homepage";
+          pname = "ravensiris-web";
 
           preInstall = ''
             ln -s ${pkgs.tailwindcss}/bin/tailwindcss _build/tailwind-${translatedPlatform}
@@ -57,6 +57,89 @@
             ${elixir}/bin/mix assets.deploy
             ${elixir}/bin/mix phx.gen.release
           '';
+        };
+        nixosModule = {
+          config,
+          lib,
+          pkgs,
+          ...
+        }: let
+          cfg = config.services.ravensiris-web;
+          user = "ravensiris-web";
+          dataDir = "/var/lib/ravensiris-web";
+        in {
+          options.services.ravensiris-web = {
+            enable = lib.mkEnableOption "ravensiris-web";
+            port = lib.mkOption {
+              type = lib.types.int;
+              default = 4000;
+              description = "Port to listen on, 4000 by default";
+            };
+            secretKeyBaseFile = lib.mkOption {
+              type = lib.types.path;
+              description = "A file contianing the Phoenix Secret Key Base. This should be secret, and not kept in the nix store";
+            };
+            databaseUrlFile = lib.mkOption {
+              type = lib.types.path;
+              description = "A file containing the URL to use to connect to the database";
+            };
+            host = lib.mkOption {
+              type = lib.types.str;
+              description = "The host to configure the router generation from";
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            assertions = [
+              {
+                assertion = cfg.secretKeyBaseFile != "";
+                message = "A base key file is necessary";
+              }
+            ];
+
+            environment.systemPackages = [default];
+
+            users.users.${user} = {
+              isSystemUser = true;
+              group = user;
+              home = dataDir;
+              createHome = true;
+            };
+            users.groups.${user} = {};
+
+            systemd.services = {
+              ravensiris-web = {
+                description = "Start up the homepage";
+                wantedBy = ["multi-user.target"];
+                script = ''
+                  # Elixir does not start up if `RELEASE_COOKIE` is not set,
+                  # even though we set `RELEASE_DISTRIBUTION=none` so the cookie should be unused.
+                  # Thus, make a random one, which should then be ignored.
+                  export RELEASE_COOKIE=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 20)
+
+                  ${default}/bin/migrate
+                  ${default}/bin/server
+                '';
+                serviceConfig = {
+                  User = user;
+                  WorkingDirectory = "${dataDir}";
+                  Group = user;
+                };
+
+                environment = {
+                  SECRET_KEY_BASE_FILE = cfg.secretKeyBaseFile;
+                  DATABASE_URL_FILE = cfg.databaseUrlFile;
+                  HOSTNAME = cfg.host;
+                  # Disable Erlang's distributed features
+                  RELEASE_DISTRIBUTION = "none";
+                  # Additional safeguard, in case `RELEASE_DISTRIBUTION=none` ever
+                  # stops disabling the start of EPMD.
+                  ERL_EPMD_ADDRESS = "127.0.0.1";
+                  # Home is needed to connect to the node with iex
+                  HOME = "${dataDir}";
+                };
+              };
+            };
+          };
         };
       };
     });
